16ebc347700244a94800dee6133fb619b6a8017c
Megan McCullough <mmccullough206@gmail.com>
2019-03-11 17:46:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,4 +6,5 @@ def display_rainbow(colors = ['red', 'orange', 'yellow', 'green',
   puts "Y: #{colors[2]}"
   puts "G: #{colors[3]}"
   puts "B: #{colors[4]}"
-  puts "I: #{colors[5]}" 
+  puts "I: #{colors[5]}"
+  puts "V: #{colors[6]}" 
